0e54ca40f5b5e9e99cf597624fa65d9c34e2985a
Alade Adeseun <aladeadeseun@gmail.com>
2026-04-27 15:03:33 +0100

    fix: has many through error on the application.

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -20,19 +20,19 @@ class Application extends Model
 
     function deployments(): HasManyThrough{
         //return $this->through("environments")->has("deployments");
-        //return $this->throughEnvironments()->hasDeployments();
-        return $this->hasManyThrough(
-            /**
-             * The first argument passed to the hasManyThrough method is the name of the final model we wish to access, 
-             * while the second argument is the name of the intermediate model.
-             */
-            Deployment::class, 
-            Environment::class, 
-            "environment_id", 
-            "id", 
-            "application_id", 
-            "id"
-        );
+        return $this->throughEnvironments()->hasDeployments();
+        // return $this->hasManyThrough(
+        //     /**
+        //      * The first argument passed to the hasManyThrough method is the name of the final model we wish to access, 
+        //      * while the second argument is the name of the intermediate model.
+        //      */
+        //     Deployment::class, 
+        //     Environment::class,
+        //     "application_id", //Foreign key on the environments table... 
+        //     "environment_id", // Foreign key on the deployments table...
+        //     "id", 
+        //     "id"
+        // );
     }
 
     function environments(): HasMany{
